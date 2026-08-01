e1290f6c5614614ad26b2c46bfdffb8458eea37a
divakar singh purva <divakarpurva04@gmail.com>
2026-08-01 08:15:08 +0530

    feat: remove background color from home page for improved design clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import { auth } from "@clerk/nextjs/server";
 export default async function Home() {
     
     await auth.protect();
-
     return (
         <>
-            <div className="bg-zinc-900 min-h-screen flex justify-center items-center text-3xl font-bold">
+            <div className="min-h-screen flex justify-center items-center text-3xl font-bold">
                 Welcome to NEGEV
             </div>
         </>
